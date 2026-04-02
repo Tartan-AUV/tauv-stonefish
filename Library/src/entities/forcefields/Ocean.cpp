57aaf3f0905af5378680601a57be0aa0b5cff0f2
Atharv Goel <atharvgo@andrew.cmu.edu>
2026-04-02 03:26:15 +0000

    i made an oopsy earlier

--- Library/src/entities/forcefields/Ocean.cpp
+++ Library/src/entities/forcefields/Ocean.cpp
@@ -1,4 +1,4 @@
-/*    
+/*
     This file is a part of Stonefish.
 
     Stonefish is free software: you can redistribute it and/or modify
@@ -43,16 +43,16 @@ Ocean::Ocean(std::string uniqueName, Scalar waves, Fluid l) : ForcefieldEntity(u
 {
     ghost->setCollisionFlags(ghost->getCollisionFlags() | btCollisionObject::CF_STATIC_OBJECT);
     oceanState = waves > Scalar(2.0) ? Scalar(2.0) : waves;
-     
+
     Scalar size(100000);
     depth = size;
     Vector3 halfExtents = Vector3(size/Scalar(2), size/Scalar(2), size/Scalar(2));
     ghost->setWorldTransform(Transform(Quaternion::getIdentity(), Vector3(0, 0, size/Scalar(2) - oceanState*Scalar(3)))); //Move ocean influence zone a bit up to account for waves
     ghost->setCollisionShape(new btBoxShape(halfExtents));
-    
+
     currents = std::vector<VelocityField*>(0);
     currentsEnabled = false;
-    
+
     liquid = l;
     wavesDebug.type = RenderableType::HYDRO_POINTS;
     wavesDebug.model = glm::mat4(1.f);
@@ -69,7 +69,7 @@ Ocean::~Ocean()
             delete currents[i];
         currents.clear();
     }
-    
+
     if(glOcean != nullptr)
         delete glOcean;
 }
@@ -91,7 +91,7 @@ Scalar Ocean::getWaterType() const
 {
     return waterType;
 }
-        
+
 OpenGLOcean* Ocean::getOpenGLOcean()
 {
     return glOcean;
@@ -116,7 +116,7 @@ VelocityField* Ocean::getVelocityField(size_t index)
 }
 
 void Ocean::setWaterType(Scalar jerlov)
-{ 
+{
     if(glOcean != nullptr)
     {
         waterType = jerlov > Scalar(1) ? Scalar(1) : (jerlov < Scalar(0) ? Scalar(0) : jerlov);
@@ -160,7 +160,7 @@ float Ocean::GetDepth(const glm::vec3& point)
     else //Flat surface
     {
         glm::vec3 wavePoint(point.x, point.y, 0.f);
-#ifdef DEBUG_WAVES  
+#ifdef DEBUG_WAVES
         wavesDebug.getDataAsPoints()->push_back(wavePoint);
 #endif
         return point.z;
@@ -176,7 +176,7 @@ Scalar Ocean::GetPressure(const Vector3& point)
 {
     Scalar g = SimulationApp::getApp()->getSimulationManager()->getGravity().getZ();
     Scalar d = GetDepth(point);
-    Scalar pressure = d < Scalar(0) ? -d*liquid.density*g : Scalar(0);
+    Scalar pressure = d > Scalar(0) ? d*liquid.density*g : Scalar(0);
     return pressure;
 }
 
@@ -219,7 +219,7 @@ void Ocean::UpdateCurrentsData()
 void Ocean::ApplyFluidForces(btDynamicsWorld* world, btCollisionObject* co, bool recompute)
 {
     Entity* ent;
-    
+
     if (btRigidBody* rb = dynamic_cast<btRigidBody*>(co))
     {
         if (rb->isStaticOrKinematicObject())
@@ -240,9 +240,9 @@ void Ocean::ApplyFluidForces(btDynamicsWorld* world, btCollisionObject* co, bool
     }
     else
         return;
-      
+
     HydrodynamicsSettings settings;
-    
+
     if (ent->getType() == EntityType::SOLID)
     {
         if(recompute)
@@ -251,7 +251,7 @@ void Ocean::ApplyFluidForces(btDynamicsWorld* world, btCollisionObject* co, bool
             settings.reallisticBuoyancy = true;
             ((SolidEntity*)ent)->ComputeHydrodynamicForces(settings, this);
         }
-        
+
         ((SolidEntity*)ent)->ApplyHydrodynamicForces();
     }
     else if (ent->getType() == EntityType::CABLE)
@@ -262,7 +262,7 @@ void Ocean::ApplyFluidForces(btDynamicsWorld* world, btCollisionObject* co, bool
             settings.reallisticBuoyancy = true;
             ((CableEntity*)ent)->ComputeHydrodynamicForces(settings, this);
         }
-        
+
         ((CableEntity*)ent)->ApplyHydrodynamicForces();
     }
 }
@@ -285,7 +285,7 @@ std::vector<Renderable> Ocean::Render()
 std::vector<Renderable> Ocean::Render(const std::vector<Actuator*>& act)
 {
     std::vector<Renderable> items(0);
-    
+
     //Update currents data
     glOceanCurrentsUBOData.gravity = glm::vec3(0.f,0.f,9.81f);
     glOceanCurrentsUBOData.numCurrents = 0;
@@ -300,7 +300,7 @@ std::vector<Renderable> Ocean::Render(const std::vector<Actuator*>& act)
                 ++glOceanCurrentsUBOData.numCurrents;
             }
     }
-    
+
     for(size_t i=0; i<act.size(); ++i)
         if(act[i]->getType() == ActuatorType::THRUSTER)
         {
@@ -310,8 +310,8 @@ std::vector<Renderable> Ocean::Render(const std::vector<Actuator*>& act)
             Vector3 thDir = -thFrame.getBasis().getColumn(0);
             Scalar R = th->getPropellerDiameter()/Scalar(2);
             Scalar vel = (th->isPropellerRight() ? Scalar(0.1) : Scalar(-0.1)) * th->getThrust();
-            glOceanCurrentsUBOData.currents[glOceanCurrentsUBOData.numCurrents].posR = glm::vec4((GLfloat)thPos.getX(), 
-                                                                             (GLfloat)thPos.getY(), 
+            glOceanCurrentsUBOData.currents[glOceanCurrentsUBOData.numCurrents].posR = glm::vec4((GLfloat)thPos.getX(),
+                                                                             (GLfloat)thPos.getY(),
                                                                              (GLfloat)thPos.getZ(), (GLfloat)R);
             glOceanCurrentsUBOData.currents[glOceanCurrentsUBOData.numCurrents].dirV = glm::vec4((GLfloat)thDir.getX(),
                                                                              (GLfloat)thDir.getY(),
